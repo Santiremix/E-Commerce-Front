f0ac67b1b04cb85d84d590c01326ca97a880d42a
Santiremix <santiagoguevara118@gmail.com>
2022-06-20 16:37:54 +0200

    prove

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Footer from "./components/Footer/Footer";
 import Login from "./components/Login/Login";
 import Profile from './components/Profile/Profile'
 import { UserProvider } from "./context/UserContext/UserState";
-import Profile from "./components/Profile/Profile";
 
 
 function App() {
