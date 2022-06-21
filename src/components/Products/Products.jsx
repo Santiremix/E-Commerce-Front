d8e8fa68e6dbdbeb35ae066e2abc6f389a2047bb
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-06-21 12:57:56 +0200

    merge resuelta

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -16,16 +16,11 @@ useEffect(() => {
   const product = products.map((product) => {
     return (
       <div key={product.id}>
-<<<<<<< HEAD
-        <span>{product.title} </span>
-        <span>{product.price}</span>
-=======
 
         <span className="productTitle"> {product.title} </span>
         <span>{product.price}</span>
         <button onClick={() => addCart(product)}>Add Cart</button>
 
->>>>>>> 073b40996e84657cb827a151fd433df42e442c91
       </div>
     );
   });
