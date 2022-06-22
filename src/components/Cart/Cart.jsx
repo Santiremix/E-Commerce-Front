a76b5c073721d19e3b2187f75f206c0fe788dce9
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-06-23 00:50:53 +0200

    renombrando clases en carrito

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,34 +2,35 @@ import { useContext, useEffect } from "react";
 import { OrdersContext } from "../../context/OrdersContext/OrderState";
 import { ProductsContext } from "../../context/ProductsContext/ProductsState";
 import { Link } from "react-router-dom";
-import "./Cart.scss"
+import "./Cart.scss";
 import { DeleteTwoTone } from "@ant-design/icons";
 
 // function summary(order) {
-//     let total = order.price 
+//     let total = order.price
 //     total = total + order.price
 //     console.log(total)
 //     return total
 // }
 
 const Cart = () => {
-  const { cart,clearCart,removeCart } = useContext(ProductsContext);
+  const { cart, clearCart, removeCart } = useContext(ProductsContext);
   const { createOrder } = useContext(OrdersContext);
-  
+
   useEffect(() => {
-      localStorage.setItem("cart", JSON.stringify(cart));
-    }, [cart]);
-  
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
   if (cart.length <= 0) {
-    return <>
-    <div className="noCart">
-        <p>Your current shopping bag is empty...</p>
-        <button className="goProducts"><Link to="/products">Check out our products</Link>
-    </button>
-    </div>
-   
-  
-    </>
+    return (
+      <>
+        <div className="noCart">
+          <p>Your current shopping bag is empty...</p>
+          <button className="goProducts">
+            <Link to="/products">Check out our products</Link>
+          </button>
+        </div>
+      </>
+    );
   }
   const createNewOrder = () => {
     createOrder(cart);
@@ -37,45 +38,49 @@ const Cart = () => {
   };
 
   const cartItem = cart.map((cartItem, i) => {
-    //   pendiente funcion suma 
+    //   pendiente funcion suma
     // const total = summary(cartItem);
-      
-   
+    console.log(cartItem.Categories[0].name);
+
     return (
-        
-      <div className="cart" key={i}>
+      <div className="cartProduct" key={i}>
         <div className="cartImage">
-            <div><img src={cartItem.image} className="cartImg"/></div>
-        <div><button className="cartRemove" onClick={() => removeCart(cartItem)}>Delete item </button></div></div>
+          <div>
+            <img src={cartItem.image} className="cartImg" />
+          </div>
+          <div>
+            <button className="cartRemove" onClick={() => removeCart(cartItem)}>
+              Delete item{" "}
+            </button>
+          </div>
+        </div>
         <div className="cartDescription">
-              <div className="cartTitle">{cartItem.name}</div>
-              <div className="cartPrice">{cartItem.price}€</div>
-            </div>
+          <div className="cartInfo">
+            <span className="name">{cartItem.name}</span>
+            <span className="category">{cartItem.Categories[0].name}</span>
+          </div>
 
-        
-    
+          <div className="cartPrice">{cartItem.price}€</div>
+        </div>
+
         {/* <button className="cartRemove" onClick={() => removeCart(cartItem)}><DeleteTwoTone twoToneColor="#000000" /></button> */}
-        
-
       </div>
     );
-    
   });
   return (
-      <div className="cartCheckout">
-    <div className="cartList">
-    <h2>Shopping Bag ({cart.length})</h2>
-      {cartItem}
-      <button onClick={() => clearCart()}>Clear cart</button>
-      
-    </div>
-    <div className="cartTotal">
+    <div className="cartCheckout">
+      <div className="cartList">
+        <h2>Shopping Bag ({cart.length})</h2>
+        {cartItem}
+        <button onClick={() => clearCart()}>Clear cart</button>
+      </div>
+      <div className="cartTotal">
         <h2>Summary</h2>
-      
-       
 
-        <button className="checkout"onClick={() => createNewOrder()}>Checkout</button>
-    </div>
+        <button className="checkout" onClick={() => createNewOrder()}>
+          Checkout
+        </button>
+      </div>
     </div>
   );
 };
