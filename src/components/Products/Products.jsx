42d0ad73fc78a6786c2118d93e0ea2532eeb1f90
Santiremix <santiagoguevara118@gmail.com>
2022-06-28 11:16:24 +0200

    cambios menores

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,4 +1,4 @@
-import e from "cors";
+// import e from "cors";
 import { useContext, useEffect, useState } from "react";
 import { ProductsContext } from "../../context/ProductsContext/ProductsState";
 import "./Products.scss"
